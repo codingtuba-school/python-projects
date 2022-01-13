b9e0d44d321dd79f4ece3b85eef436908cf3ec46
aimedtuba-school <aimedtuba@gmail.com>
2022-01-13 08:05:00 +0700

    scraping update 📈

--- scraping.py
+++ scraping.py
@@ -28,17 +28,53 @@ def is_good_response(resp):
 def log_error(e):
     print(e)
 
+url="https://bbc.com"
+select=".media__title"
+
 def display_headlines():
     global mylist
     mylist.destroy()
     mylist=Listbox(root, yscrollcommand = scrollbar.set,width=50,height=50)
     mylist.pack( side = LEFT, fill = BOTH )
     scrollbar.config( command = mylist.yview )
-    for _,i in enumerate(BeautifulSoup(simple_get('https://bbc.com'),'html.parser').select('.media__title')): # media__title
+    for _,i in enumerate(BeautifulSoup(simple_get(url),'html.parser').select(select)): # media__title
         mylist.insert(END,i.text.replace("  ",""))
+def options():
+    tp=Toplevel()
+
+    def o_save():
+        global url,select
+
+        url=ent1.get()
+        select=ent2.get()
+        tp.destroy()
+
+    frms=[
+        Frame(tp),
+        Frame(tp),
+        Frame(tp)
+    ]
+    for frm in frms:
+        frm.pack(expand=True)
+
+    Label(frms[0],text="url: ").grid(row=0,column=0)
+    ent1=Entry(frms[0])
+    ent1.grid(row=0,column=1)
+
+    Label(frms[1],text="select: ").grid(row=0,column=0)
+    ent2=Entry(frms[1])
+    ent2.grid(row=0,column=1)
+
+    Button(frms[2],text="save",command=o_save).grid(row=0,column=0)
+    Button(frms[2],text="close",command=lambda tp=tp:tp.destroy()).grid(row=0,column=1)
+
+    tp.mainloop()
 
 root=Tk()
-Button(text="scrape",command=display_headlines).pack()
+frame=Frame()
+frame.pack(expand=True)
+Button(frame,text="scrape",command=display_headlines).grid(row=0,column=0)
+Button(frame,text="options",command=options).grid(row=0,column=1)
 
 scrollbar = Scrollbar(root)
 scrollbar.pack( side = RIGHT, fill=Y )
